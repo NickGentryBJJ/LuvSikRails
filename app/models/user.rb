67198fc4e017a4d587341d5bd93b440ef250fd0a
nj <nickgentrymma@gmail.com>
2024-03-15 05:54:48 -0700

    added user seed and completed model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,47 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+  validates :username, uniqueness: true, length: { in: 3..30 }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..255 }, allow_nil: true  
   has_secure_password
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.authenticate(password)
+        return user 
+    else
+        return nil 
+    end
+  end
+  
+  
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save! 
+    self.session_token 
+  end
+  
+  private
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+  
+  def generate_unique_session_token
+    while true
+      token = SecureRandom::urlsafe_base64(16)
+      return token unless User.exists?(session_token: token)
+    end
+  end
+
 end
