fcb528577d98cb6162dfac2b2d6ed85301f6ce83
nj <nickgentrymma@gmail.com>
2024-03-15 06:02:05 -0700

    added users controller and session controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,28 @@
+class Api::SessionsController < ApplicationController
+  def show
+    @user = current_user
+    if @user 
+      render 'api/users/show'
+    else
+      render json: { user: nil }
+    end
+    
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:username], params[:password])
+    if @user 
+      login!(@user)
+      render 'api/users/show'
+    else 
+      render json: {errors: ['The provided credentials were invalid.']}, status: 422
+    end
+    
+  end
+
+  def destroy
+    logout! 
+    render json: {message: ['Success']}
+  end
+
+end
