8cee1fbca9f6e370dbb95aaa74e0a8ed80a1b7e6
nj <nickgentrymma@gmail.com>
2024-03-16 23:15:12 -0700

    added shows controller

--- app/controllers/api/shows_controller.rb
+++ app/controllers/api/shows_controller.rb
@@ -1,17 +1,42 @@
 class Api::ShowsController < ApplicationController
     wrap_parameters include: Show.attribute_names + ['venue', 'location']
-    
+
+    def index 
+        @shows = Show.all 
+        render :index
+    end
+
+    def show
+        @show = Show.find(params[:id])
+        render :show
+    end
+
+    def update
+        @show = Show.find(params[:id])
+        if @show.update(show_params)
+            @shows = Show.all 
+            render json: :index
+        else  
+            render json: {errors:@event.errors.full_messages},status: 422
+        end
+    end
+
+    def destroy
+        @show = Show.find(params[:id])
+        @show.destroy
+    end
+
     def create
         @show = Show.new(show_params)
         if @show.save 
             render :show
         else 
-            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+            render json: { errors: @show.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
     private
     def show_params
-        params.require(:user).permit(:venue, :location, :date)
+        params.require(:show).permit(:venue, :location, :date)
     end
-  end
+end
